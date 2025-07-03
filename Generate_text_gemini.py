1c416e1023375acbb825f03220334c16d4f2d4f5
Harinit99 <harini9899@gmail.com>
2025-07-03 12:35:16 +0530

    updated

--- Generate_text_gemini.py
+++ Generate_text_gemini.py
@@ -1,12 +1,7 @@
 from google import genai
 
 client = genai.Client(api_key="AIzaSyAHdrg4G4Ves-qh8GcNfwh0FiPIGRCsGic")
-failure = ""
-AC = ""
-
 prompt = f'''
-
-No more explanation
 '''
 response = client.models.generate_content(model="gemini-2.0-flash", contents=prompt)
 print(response.text)
